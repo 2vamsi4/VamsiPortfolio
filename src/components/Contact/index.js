bf6e5b3173cf78921ff2fd0a4b16b0243c639dc6
2vamsi4 <katurivamsi4@gmail.com>
2024-11-24 22:07:38 +0530

    ADDED EMAIL

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_p4i4j2d', 'template_vvs3n0q', form.current, 'jli4dc_jlIXL6A6qh')
       .then((result) => {
         setOpen(true);
         form.current.reset();
@@ -138,9 +138,6 @@ const Contact = () => {
         console.log(error.text);
       });
   }
-
-
-
   return (
     <Container>
       <Wrapper>
